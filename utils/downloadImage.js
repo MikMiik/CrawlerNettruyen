0df4dae73ff47232afba07cca15ffc6b4d073a23
MikMik <minh0936532430@gmail.com>
2025-08-12 11:04:05 +0700

    update

--- utils/downloadImage.js
+++ utils/downloadImage.js
@@ -1,8 +1,15 @@
 const axios = require("axios");
 const fs = require("fs");
+const path = require("path");
 
 async function downloadImage(url, filePath, referer) {
   try {
+    // Tạo folder nếu chưa tồn tại
+    const dir = path.dirname(filePath);
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+
     const response = await axios({
       url,
       method: "GET",
