27641a427df7c2ffe3a003d49055584ce7507cd1
MikMik <minh0936532430@gmail.com>
2025-08-15 15:06:52 +0700

    update

--- crawlPages.js
+++ crawlPages.js
@@ -30,10 +30,10 @@ const scrollToBottom = async (page, step = 150, delay = 30) => {
 const startCrawling = async (urlsIds) => {
   const cluster = await Cluster.launch({
     concurrency: Cluster.CONCURRENCY_PAGE,
-    maxConcurrency: 2,
+    maxConcurrency: 5,
     puppeteer: puppeteerExtra,
     puppeteerOptions: {
-      headless: false,
+      headless: true,
       defaultViewport: false,
     },
     args: [
