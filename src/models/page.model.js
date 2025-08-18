df2cf8b940e5b30e446aafe8ed7de0c93019da46
MikMik <minh0936532430@gmail.com>
2025-08-18 23:51:36 +0700

    update

--- src/models/page.model.js
+++ src/models/page.model.js
@@ -2,9 +2,7 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Page extends Model {
-    static associate(models) {
-      Page.belongsTo(models.Chapter, { foreignKey: "chapterId" });
-    }
+    static associate(models) {}
   }
   Page.init(
     {
